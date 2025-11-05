12788d083c057619f15525eda54a3ef174baf4e1
Aldi Pasca <aldipascaa@gmail.com>
2025-11-05 14:09:58 +0700

    MyCalculator commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,11 @@
+﻿using MyCalculator;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();  
+        int result = calculator.Add(3,4);
+        Console.WriteLine(result);
+    }
+}
