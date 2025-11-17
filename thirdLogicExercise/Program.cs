deb405b1620fe937712e1e0a5378d23b476fb730
Aldi Pasca <aldipascaa@gmail.com>
2025-11-17 11:40:37 +0700

    Day8Exercise

---
+++ thirdLogicExercise/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel;
+using System.Linq.Expressions;
+namespace LogicExercise;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Pick a number!!");
+        int x = Convert.ToInt32(Console.ReadLine());
+        //FirstLogic(x);
+        //Console.WriteLine();
+        //SecondLogic(x);
+        //Console.WriteLine();
+        ThirdLogic(x);
+    }
+
+    static void FirstLogic(int x)
+    {
+        for (int i = 1; i <= x;i++)
+        {
+            if (i % 3 == 0 & i % 5 == 0){
+                Console.Write("foobar");
+            }
+            else if (i % 3 == 0){
+                Console.Write("foo");
+            }
+            else if (i % 5 == 0){
+                Console.Write("bar");
+            }
+            else Console.Write(i);
+            if (i != x)
+            {
+            Console.Write(",");
+            }
+        }
+    }
+
+    static void SecondLogic(int x)
+    {
+        for (int i = 1; i <= x; i++)
+        {
+            bool logic1 = i % 3 == 0;
+            bool logic2 = i % 5 == 0;
+            bool logic3 = i % 7 == 0;
+            Console.Write((logic1 ? "foo" : "") + (logic2 ? "bar" : "") + (logic3 ? "jazz" : ""));
+            if (i!=x)
+                Console.Write(logic1 || logic2 || logic3?",": $"{i},");
+        }
+    }
+
+    static void ThirdLogic(int x)
+    {
+        for (int i = 1; i <= x; i++)
+        {
+            var dictionary = new Dictionary<int, string>()
+            {
+                {3, "foo"},
+                {5, "bar"},
+                {7, "jazz"},
+                {9, "bazz"}
+            };
+            string output = "";
+            foreach (var (key,value) in dictionary)
+            {
+                output += i % key == 0 ? value : "";
+            }
+            if (i != x)
+                output += output != "" ? "," : $"{i},";
+            Console.Write(output);
+        }
+    }
+}
